874d45fe97a21e1e79c9bbfea190bcdf3df78f60
Diogo Treml <tremldiogo@gmail.com>
2024-04-25 23:17:05 -0300

    stuff

--- src/controllers/personagemController.js
+++ src/controllers/personagemController.js
@@ -17,15 +17,58 @@ export async function getPersonagens() {
 
 export async function getPersonagemById(id) {
   try {
-    const query = "SELECT * FROM personagens WHERE id=?";
-    const data = await executeQuery(mysql.format(query, id));
+    const personagemQuery = `
+    SELECT p.*, 
+           c.nome AS nome_classe, c.descricao AS descricao_classe, 
+           r.nome AS nome_raca, r.descricao AS descricao_raca
+    FROM personagens p
+    JOIN classe c ON p.classe_id = c.id
+    JOIN raca r ON p.raca_id = r.id
+    WHERE p.id = ?;
+  `;
+    const personagem = await executeQuery(mysql.format(personagemQuery, [id]));
+    if (personagem.length === 0) {
+      return {
+        status: 204,
+        data: null,
+      };
+    }
+
+    const habilidadesQuery = `
+      SELECT h.nome, h.descricao
+      FROM habilidade h
+      JOIN personagem_habilidades ph ON h.id = ph.habilidade
+      WHERE ph.personagem = ?;
+    `;
+    const habilidades = await executeQuery(
+      mysql.format(habilidadesQuery, [id])
+    );
+    const { nome, nome_classe, descricao_classe, nome_raca, descricao_raca } =
+      personagem[0];
+
+    const dataRes = {
+      nome: nome,
+      classe: {
+        nome: nome_classe,
+        descricao: descricao_classe,
+      },
+      raca: {
+        nome: nome_raca,
+        descricao: descricao_raca,
+      },
+      habilidades: habilidades,
+    };
 
     return {
-      status: data.length > 0 ? 200 : 204,
-      data: data.length > 0 ? data[0] : null,
+      status: 200,
+      data: dataRes,
     };
   } catch (err) {
-    return statusErro;
+    console.error(err);
+    return {
+      status: 500,
+      data: { message: "Erro ao processar a requisição" },
+    };
   }
 }
 
@@ -34,11 +77,11 @@ export async function postPersonagem({ nome, classe, raca, habilidades }) {
   try {
     await connection.beginTransaction();
 
-    const classeExists = await connection.query(
+    const [classeExists] = await connection.query(
       `SELECT id FROM classe WHERE id = ?`,
       [classe]
     );
-    const racaExists = await connection.query(
+    const [racaExists] = await connection.query(
       `SELECT id FROM raca WHERE id = ?`,
       [raca]
     );
@@ -51,14 +94,14 @@ export async function postPersonagem({ nome, classe, raca, habilidades }) {
     }
 
     const personagemResult = await connection.query(
-      `INSERT INTO personagens (nome, classe, raca) VALUES (?, ?, ?)`,
+      `INSERT INTO personagens (nome, classe_id, raca_id) VALUES (?, ?, ?)`,
       [nome, classe, raca]
     );
-    const personagemId = personagemResult.insertId;
+    const personagemId = personagemResult[0].insertId;
 
     for (const habilidade of habilidades) {
-      const habilidadeExists = await connection.query(
-        `SELECT id FROM habilidades WHERE id = ?`,
+      const [habilidadeExists] = await connection.query(
+        `SELECT id FROM habilidade WHERE id = ?`,
         [habilidade]
       );
       if (habilidadeExists.length === 0) {
@@ -75,7 +118,7 @@ export async function postPersonagem({ nome, classe, raca, habilidades }) {
     return { status: 201, data: { message: "Registro criado." } };
   } catch (error) {
     await connection.rollback();
-    return { status: 500, data: { message: "Erro desconhecido." } };
+    return { status: 500, data: { message: error.message } };
   } finally {
     connection.release();
   }
@@ -111,7 +154,7 @@ export async function putPersonagem({ id, nome, classe, raca, habilidades }) {
     }
 
     await connection.query(
-      `UPDATE personagens SET nome = ?, classe = ?, raca = ? WHERE id = ?`,
+      `UPDATE personagens SET nome = ?, classe_id = ?, raca_id = ? WHERE id = ?`,
       [nome, classe, raca, id]
     );
 
@@ -122,7 +165,7 @@ export async function putPersonagem({ id, nome, classe, raca, habilidades }) {
 
     const habilidadePromises = habilidades.map(async (habilidade) => {
       const [habilidadeExists] = await connection.query(
-        `SELECT id FROM habilidades WHERE id = ?`,
+        `SELECT id FROM habilidade WHERE id = ?`,
         [habilidade]
       );
       if (habilidadeExists.length === 0) {
